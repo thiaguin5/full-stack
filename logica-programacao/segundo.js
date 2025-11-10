8b35cb4da1d927fecd6c9260abd032a87271d475
thiaguin5 <eduardo.arocha2024@gmail.com>
2025-11-10 15:57:47 -0300

    add soma,subtração,divisão e multiplicacão

--- logica-programacao/segundo.js
+++ logica-programacao/segundo.js
@@ -1,5 +1,12 @@
-let numero1 = prompt("digite um numero")
-let numero2 = prompt("digite outro número")
+let numero1 = Number(prompt("digite um numero"))
+let numero2 = Number(prompt("digite outro número"))
 
-console.log (numero1 + numero2)
+//soma
+console.log("soma: " + numero1 + numero2)
+//sub
+console.log("subtração: " , numero1 - numero2)
+//mult
+console.log("multiplicação: " + numero1 * numero2)
+//div
+console.log("divisão: " + numero1 / numero2)
 
