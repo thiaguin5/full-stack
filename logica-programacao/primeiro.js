d7d4b8dc2e8bae98009af0d76d8a7e57c26bc6dd
thiaguin5 <eduardo.arocha2024@gmail.com>
2025-11-10 15:11:01 -0300

    add js

---
+++ logica-programacao/primeiro.js
@@ -0,0 +1,15 @@
+console.log("ola thiago");
+//perguntando o nome para o usuário
+const username = prompt("qual é o seu nome?")
+
+//perguntando qual é sua idade
+const userage = prompt("qual é sua idade")
+
+
+//imprimindo na tela o resultado
+document.write("seu nome é:" + "<strong>" + username + "</strong>" + "<br>");
+
+//imprimindo na tela a idade
+document.write("sua idade é:" + "<strong>" + userage+ "</strong>" + "anos");
+
+
---
+++ logica-programacao/segundo.js
@@ -0,0 +1,5 @@
+let numero1 = prompt("digite um numero")
+let numero2 = prompt("digite outro número")
+
+console.log (numero1 + numero2)
+
