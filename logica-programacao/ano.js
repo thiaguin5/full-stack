27c40078a49f97133eedaee3e6a4ce1ca0e21a03
thiaguin5 <eduardo.arocha2024@gmail.com>
2025-11-10 16:25:57 -0300

    adicionando logica de programacao de ano e copa

---
+++ logica-programacao/ano.js
@@ -0,0 +1,14 @@
+//criar um programa, para verificar o ano de nascimento
+const anoatual = 2025;
+
+let numero1 = Number(prompt("digite sua idade"))
+let numero2 = Number(prompt("digite o ano ano tual"))
+
+//sub
+console.log("voce nasceu em : " , numero1 - numero2)
+
+
+
+
+
+
---
+++ logica-programacao/copa.js
@@ -0,0 +1,8 @@
+//criar um programa, para verificar a proxima copa do mundo
+const anoatual = 2025;
+
+let numero1 = Number(prompt("digite o ano que foi a ultima copa "))
+let numero2 = Number(prompt("adicione mais quatro anos"))
+
+//sub
+console.log(" o ano que acontece a proxima copa do mundo é: " , numero1 + numero2)
