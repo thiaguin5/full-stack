29b74e6d11f016f1485f0862e7fd6cb4930ac837
thiaguin5 <eduardo.arocha2024@gmail.com>
2025-11-18 15:04:02 -0300

    else e if

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,13 +1,50 @@
+const form = document.querySelector ("form")
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
-const inputTel = document.getElementById("telefone")
-// inputEmail
-// inputTel
+const inputTel = document.getElementById("telefone");
+
+form.addEventListener("submit" , function(event) {
+     event.preventDefault();
 
 
-function btnCadastrar(event) {
-    event.preventDefault();
-    console.log(inputNome.value);
-    console.log(inputEmail.value);
-    console.log(inputTel.value);
+
+//validação do formulário
+/*
+
+se o input for vazio,adicionar uma mensagem
+
+*/
+
+     console.log("Nome", inputNome.value)
+     console.log("Email", inputEmail.value)
+     console.log("telefone", inputTel.value)
+     
+})
+
+if (inputNome.value =="") {
+alert("preencha a caixa")
+return false;
+
 }
+
+if (inputEmail.value =="") {
+alert("preencha a caixa")
+return false;
+
+}
+
+if (inputTel.value =="") {
+alert("preencha a caixa")
+return false;
+
+
+}
+
+
+
+// function btnCadastrar(event) {
+//    event.preventDefault();
+//    console.log(inputNome.value);
+//  console.log(inputEmail.value);
+//    console.log(inputTel.value);
+// }
