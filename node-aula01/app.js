1dae472c3aa8a32ff8954e0b366014b21d7cc3ea
thiaguin5 <eduardo.arocha2024@gmail.com>
2025-11-24 16:23:38 -0300

    subindo arquivo node

--- node-aula01/app.js
+++ node-aula01/app.js
@@ -2,11 +2,31 @@ const express = require ("express")
 const app = express ()
 const port = 3000;
 
+const produtos = [
+    {id: 1, nome: 'produto a', preco:100},
+    {id: 2, nome: 'produto b', preco:150},
+    {id: 3, nome: 'produto c', preco:200},
+]
+
+
+
 //criar função
 //rota principal
 app.get('/',(req, res) => {
     res.send('bem vindo ao servidor Node.js');
 
+})
+
+// rota produtos
+app.get('/produtos', (req, res) => {
+    res.send(produtos)
+
+})
+
+//rota home
+app.get('/home,', (req, res) => {   
+    //console.log("olá",__dirname + '')
+    res.sendFile(__dirname + '/public/home.html')   
 })
 
 //executando o servidor
