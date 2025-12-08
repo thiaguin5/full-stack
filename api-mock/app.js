c5337d4ab5866b55f3f492ad2a9c3295c00369d0
thiaguin5 <eduardo.arocha2024@gmail.com>
2025-12-08 14:58:52 -0300

    add rota selecoes

--- api-mock/app.js
+++ api-mock/app.js
@@ -4,8 +4,22 @@ const app = expresss();
 
 app.get ("/", (req, res) => {
 
-    res.send("ola node .js");
+    res.send("ola copa-mundo");
+
+    
 })
+
+//buscar todas as seleções 
+app.get('selecoes', (req, res) => {
+
+    const sql = 'SELECT * FROM selecoes';
+
+    conexão.query(sql, (erro, result) => {
+        result.json(result);
+    })
+});
+
+
 
 export default app;
 
--- api-mock/infra/conexao.js
+++ api-mock/infra/conexao.js
@@ -1,21 +1,13 @@
 import mysql from 'mysql2';
 
-app.get ("/", (req, res) => {
-    res.send("ola node .js");
-})
-
-
 const conexao = mysql.createConnection({
-    host:'localhost',
-    port:'3306',
-    user:'root',
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
     password: 'root',
     database: 'copa_mundo'
-
 });
 
 conexao.connect();
-    
-export default conexao;
 
-    
+export default conexao;
