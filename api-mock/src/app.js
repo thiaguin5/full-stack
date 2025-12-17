87aa1376590360ecb8f6e7d752859f25fca50f2f
thiaguin5 <eduardo.arocha2024@gmail.com>
2025-12-17 16:13:10 -0300

    add put e post

--- api-mock/src/app.js
+++ api-mock/src/app.js
@@ -1,106 +1,67 @@
-import expresss from "express"
-const app = expresss();
+import express from "express";
+import conexao from "../infra/conexao.js";
 
-app.get ("/", (req, res) => {
+const app = express();
 
-    res.send("ola node .js");
+// Indicar para express ler o body com json
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Olá Copa do Mundo!");
+});
+
+// Buscar todas as seleções 
+app.get('/selecoes', (req, res) => {
+    const sql = "select * from selecoes";
+
+    conexao.query(sql, (erro, result) => {
+        res.json(result);
+    });
+});
+
+// Buscando por ID
+app.get('/selecoes/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "select * from selecoes where id=?;";
+
+    conexao.query(sql, id, (erro, result) => {
+        res.json(result[0]);
+    });
+});
+
+// Criando POST para cadastrar 
+app.post('/selecoes', (req, res) => {
+    const selecao = req.body;
+    const sql = "INSERT INTO selecoes SET?;";
+
+    conexao.query(sql, selecao, () => {
+        res.json({ mensagem: "Cadastrado com sucesso!"});
+    })
+});
+
+
+
+// Deletando registro
+app.delete('/selecoes/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "delete from selecoes where id=?";
+
+    conexao.query(sql, id, () => {
+        res.json({ menssagem: "Deletando com sucesso"});
+    });
 })
+
+
+app.put('/selecoes/:id', (req, res) => {
+
+    const id = req.params.id;
+    const selecao = req.body;
+    const sql = "update selecoes set ? where id=?";
+
+    conexao.query(sql, [selecao, id], () => {
+        res.json({ mensagem: "Atualizado com sucesso!"});
+    });
+});
+
 
 export default app;
-
-
-//------------CONST DE CADASTROS-----------------
-
-
-const cadastros = [
-{
-id: 1,
-nome: "Fernanda Silva",
-telefone: "11987654321",
-cpf: "123.456.789-00",
-email: "fernanda@gmail.com",
-idade: 28,
-endereco: "Rua das Flores, 123"
-},
-{
-id: 2,
-nome: "Caio Oliveira",
-telefone: "11922223333",
-cpf: "987.654.321-11",
-email: "caio@gmail.com",
-idade: 32,
-endereco: "Avenida Brasil, 450"
-},
-{
-id: 3,
-nome: "Pedro Almeida",
-telefone: "21999998888",
-cpf: "321.654.987-55",
-email: "pedro@hotmail.com",
-idade: 45,
-endereco: "Rua Central, 999"
-}
-];
-
-
-//------------FUNÇÕES DE CADASTRO-----------------
-
-function buscarTodosCadastros() {
-    return cadastros;
-}
-
-function buscarCadastroPorId(id) {
-    return cadastros.findIndex(cadastro => cadastro.id === id);
-}
-
-
-//------------ROTAS DE CADASTRO-----------------
-
-
-//rota principal
-app.get("/", (req, res) => {
-  res.send("Rota principal");
-});
-
-
-//rota para buscar todos os cadastros
-app.get("/cadastros", (req, res) => {
-    res.json(cadastros);
-})
-
-//rota para buscar cadastro por id
-app.get("/cadastros/:id", (req, res) => {
-  res.json(buscarCadastroPorId(req.params.id));
-});
-
-
-// EDITAR cadastro por ID
-
-app.put("/cadastros/:id", (req, res) => {
-  let index = buscarIdCadastros(req.params.id);
-
-  if (index === -1) {
-    return res.status(404).send(`Cadastro não encontrado!`);
-  }
-
-  cadastros[index].nome = req.body.nome;
-  cadastros[index].telefone = req.body.telefone;
-  cadastros[index].cpf = req.body.cpf;
-  cadastros[index].email = req.body.email;
-  cadastros[index].idade = req.body.idade;
-  cadastros[index].endereco = req.body.endereco;
-
-  res.json(cadastros[index]);
-});
-
-
-//rota para deletar cadastro por id
-app.delete("/cadastros/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const indice = buscarCadastroPorId(id);
-})
-
-app.post("/cadastros", (req, res) => {
-  cadastros.push(req.body);
-  res.status(201).send("Cadastro realizado com sucesso!");
-});
